1a1e8e5ca982a1ce0fe12f191d037a5d37ce5d7e
Ben <benknight135@gmail.com>
2021-12-18 02:49:36 +0000

    add favicon

--- app.js
+++ app.js
@@ -1,26 +1,28 @@
 const express = require('express');
 var enforce = require('express-sslify');
+var favicon = require('serve-favicon');
 const app = express();
 const path = require('path');
 const router = express.Router();
 
 app.use(enforce.HTTPS({ trustProtoHeader: true }));
+app.use(favicon(path.join(__dirname,'images','favicon.ico')));
 
 // serve index.html
 router.get('/',function(req,res){
-  res.sendFile(path.join(__dirname+'/web/index.html'));
+  res.sendFile(path.join(__dirname,'web','index.html'));
 });
 router.get('/ar-pattern',function(req,res){
-  res.sendFile(path.join(__dirname+'/web/ar-pattern.html'));
+  res.sendFile(path.join(__dirname,'web','ar-pattern.html'));
 });
 router.get('/ar-gps',function(req,res){
-  res.sendFile(path.join(__dirname+'/web/ar-gps.html'));
+  res.sendFile(path.join(__dirname,'web','ar-gps.html'));
 });
 
 // define static data folders
-app.use('/images', express.static(__dirname + '/images'));
-app.use('/markers', express.static(__dirname + '/markers'));
-app.use('/models', express.static(__dirname + '/models'));
+app.use('/images', express.static(path.join(__dirname,'/images')));
+app.use('/markers', express.static(path.join(__dirname,'/markers')));
+app.use('/models', express.static(path.join(__dirname,'/models')));
 
 //add the router
 app.use('/', router);
