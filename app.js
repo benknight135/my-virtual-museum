abf4faed98ebbaf9517b7d94778de8be7c375d9e
Ben <benknight135@gmail.com>
2021-12-18 18:54:59 +0000

    add template for hunt selection

--- app.js
+++ app.js
@@ -6,31 +6,46 @@ const path = require('path');
 const router = express.Router();
 
 app.use(enforce.HTTPS({ trustProtoHeader: true }));
-app.use(favicon(path.join(__dirname,'images','favicon.ico')));
+app.use(favicon(path.join(__dirname,'data','images','favicon.ico')));
 
 // serve index.html
 router.get('/',function(req,res){
   res.sendFile(path.join(__dirname,'web','index.html'));
 });
-router.get('/ar-hunt',function(req,res){
-  res.sendFile(path.join(__dirname,'web','ar-hunt.html'));
-});
-router.get('/ar-gps',function(req,res){
-  res.sendFile(path.join(__dirname,'web','ar-gps.html'));
+router.get('/ar-pattern',function(req,res){
+  res.sendFile(path.join(__dirname,'web','ar-pattern.html'));
 });
 
 // templates
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 
-app.get("/test", (req, res) => {
-  res.render("index", { title: "Home" });
+// load treasure hunt templates
+var treasure_hunts = {
+  "hunts":[
+    {
+      "name": "Barden Lake",
+      "link": "/barden-lake-ar-hunt"
+    }
+  ]
+}
+app.get("/ar-hunt-select", (req, res) => {
+  res.render("ar-hunt-select", treasure_hunts);
+});
+app.post("/load-ar-hunt", (req, res) => {
+  //TODO: choose which ar hunt to load
+  res.redirect('/barden-lake-ar-hunt');
+});
+
+var barden_lake_hunt = require(path.join(__dirname,'data','treasure_hunts','barden_lake.json'));
+app.get("/barden-lake-ar-hunt", (req, res) => {
+  res.render("ar-hunt", barden_lake_hunt);
 });
 
 // define static data folders
-app.use('/images', express.static(path.join(__dirname,'/images')));
-app.use('/markers', express.static(path.join(__dirname,'/markers')));
-app.use('/models', express.static(path.join(__dirname,'/models')));
+app.use('/images', express.static(path.join(__dirname,'data','images')));
+app.use('/markers', express.static(path.join(__dirname,'data','markers')));
+app.use('/models', express.static(path.join(__dirname,'data','models')));
 
 //add the router
 app.use('/', router);
